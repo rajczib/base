68dc28921da951e74f1d0e66068145a551e98c34
rajczib <rajczi.barnabass@gmail.com>
2023-03-20 15:13:53 +0100

    +1 unit test added

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,12 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
+	@Test
+	public void needsOneMoreTest()
+	{
+		int valami = 5;
+		Assert.assertEquals(5, valami);
+	}
+
 	
 }
