98fb48be36da3f89fa6b4e099574d35af7363676
rajczib <rajczi.barnabass@gmail.com>
2023-03-20 14:35:37 +0100

    branch-B modified

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -12,7 +12,7 @@ public class TrainSystem {
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
-
+	public long mas_valami; 
 	public TrainController getController() {
 		return controller;
 	}
